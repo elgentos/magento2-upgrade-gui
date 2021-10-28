191f03c4a7687f47e67a84fc08127dd697d5dc9b
Jesse de Boer <jesse@elgentos.nl>
2021-10-28 16:06:20 +0200

    Fix for slash

--- src/components/OverrideSelector.vue
+++ src/components/OverrideSelector.vue
@@ -1,54 +1,85 @@
 <template>
   <div v-if="overrides">
     <div>
-      <select @change="loadOverride" id="override" class="-mr-2 rounded-md mt-1 form-select block w-full -ml-1 pl-3 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5 bg-indigo-200">
+      <select
+        @change="loadOverride"
+        id="override"
+        class="block w-full py-2 pl-3 mt-1 -ml-1 -mr-2 text-base leading-6 bg-indigo-200 border-gray-300 rounded-md form-select focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+      >
         <option>Select a preference/plugin/override</option>
-        <option v-for="(override, index) in overrides" :key="index" :value="index">{{ override[0] }}: {{ override[1] }} {{ override[2] }}</option>
+        <option
+          v-for="(override, index) in overrides"
+          :key="index"
+          :value="index"
+        >
+          {{ override[0] }}: {{ override[1] }} {{ override[2] }}
+        </option>
       </select>
     </div>
 
-    <span class="relative z-0 inline-flex shadow-sm rounded-md my-4" v-if="selectedOverride">
-      <button type="button" class="ml-2 bg-gray-400 relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-900 hover:text-gray-900 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+    <span
+      class="relative z-0 inline-flex my-4 rounded-md shadow-sm"
+      v-if="selectedOverride"
+    >
+      <button
+        type="button"
+        class="relative inline-flex items-center px-4 py-2 ml-2 text-sm font-medium leading-5 text-gray-900 transition duration-150 ease-in-out bg-white bg-gray-400 border border-gray-300 rounded-l-md hover:text-gray-900 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700"
+      >
         Previous (←)
       </button>
-      <button type="button" class="bg-green-400 -ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-900 hover:text-green-900 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+      <button
+        type="button"
+        class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium leading-5 text-gray-900 transition duration-150 ease-in-out bg-white bg-green-400 border border-gray-300 hover:text-green-900 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700"
+      >
         Mark as Fixed (F)
       </button>
-      <button type="button" class="bg-green-200 -ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-900 hover:text-green-900 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+      <button
+        type="button"
+        class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium leading-5 text-gray-900 transition duration-150 ease-in-out bg-white bg-green-200 border border-gray-300 hover:text-green-900 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700"
+      >
         Mark as False Positive (P)
       </button>
-      <button type="button" class="bg-orange-200 -ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-900 hover:text-orange-900 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+      <button
+        type="button"
+        class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium leading-5 text-gray-900 transition duration-150 ease-in-out bg-white bg-orange-200 border border-gray-300 rounded-r-md hover:text-orange-900 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700"
+      >
         Mark as Fix later (L)
       </button>
-      <button type="button" class="bg-red-200 -ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-900 hover:text-red-900 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+      <button
+        type="button"
+        class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium leading-5 text-gray-900 transition duration-150 ease-in-out bg-white bg-red-200 border border-gray-300 rounded-r-md hover:text-red-900 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700"
+      >
         Mark as Cannot fix (N)
       </button>
-      <button type="button" class="bg-gray-200 -ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-900 hover:text-gray-900 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+      <button
+        type="button"
+        class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium leading-5 text-gray-900 transition duration-150 ease-in-out bg-white bg-gray-200 border border-gray-300 rounded-r-md hover:text-gray-900 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700"
+      >
         Next (→)
       </button>
     </span>
 
     <div class="flex flex-row mt-2" v-if="selectedOverride">
-      <div class="w-1/2 bg-indigo-200 px-4 py-2 overflow-y-scroll">
+      <div class="w-1/2 px-4 py-2 overflow-y-scroll bg-indigo-200">
         <h2 class="mb-4 font-bold">Diff</h2>
         <div>
           <prism-editor
-              class="my-editor vendor-file-content-editor height-300"
-              v-model="vendorFileContent"
-              :highlight="vendorFileContentHighlighter"
-              :line-numbers="lineNumbers"
+            class="my-editor vendor-file-content-editor height-300"
+            v-model="vendorFileContent"
+            :highlight="vendorFileContentHighlighter"
+            :line-numbers="lineNumbers"
           ></prism-editor>
         </div>
       </div>
-      <div class="w-1/2 bg-indigo-300 px-4 py-2 overflow-y-scroll">
+      <div class="w-1/2 px-4 py-2 overflow-y-scroll bg-indigo-300">
         <h2 class="mb-4 font-bold">{{ type }}</h2>
         <h3 class="mb-4 font-bold">{{ copyPasteableFilePath }}</h3>
-        <div >
+        <div>
           <prism-editor
-              class="my-editor custom-file-content-editor height-300"
-              v-model="customFileContent"
-              :highlight="customFileContentHighlighter"
-              :line-numbers="lineNumbers"
+            class="my-editor custom-file-content-editor height-300"
+            v-model="customFileContent"
+            :highlight="customFileContentHighlighter"
+            :line-numbers="lineNumbers"
           ></prism-editor>
         </div>
       </div>
@@ -77,14 +108,14 @@
 </style>
 <style>
 span.function.token.methodHighlight {
-  background-color:yellow;
-  border:3px solid red;
-  padding:5px;
+  background-color: yellow;
+  border: 3px solid red;
+  padding: 5px;
 }
 </style>
 
 <script>
-import { ipcRenderer } from 'electron';
+import { ipcRenderer } from "electron";
 import { PrismEditor } from "vue-prism-editor";
 import "vue-prism-editor/dist/prismeditor.min.css"; // import the styles somewhere
 import { highlight, languages } from "prismjs/components/prism-core";
@@ -96,7 +127,7 @@ import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-php";
 import "prismjs/themes/prism-tomorrow.css"; // import syntax highlighting styles
 
-const fs = require('fs');
+const fs = require("fs");
 
 export default {
   components: {
@@ -104,37 +135,42 @@ export default {
   },
   data() {
     return {
-      type: '',
-      copyPasteableFilePath: '',
+      type: "",
+      copyPasteableFilePath: "",
       classMap: null,
       overrides: null,
       selectedOverride: null,
       vendorCheckDiffs: [],
-      vendorFileContent: '',
-      customFileContent: '',
-      vendorFileType: 'diff',
-      customFileType: 'diff',
+      vendorFileContent: "",
+      customFileContent: "",
+      vendorFileType: "diff",
+      customFileType: "diff",
       selectedMagento2ProjectDir: null,
       lineNumbers: true,
-      methodName: null
-    }
+      methodName: null,
+    };
   },
   mounted() {
-    ipcRenderer.on('overridesParsed', (event, args) => {
+    ipcRenderer.on("overridesParsed", (event, args) => {
       this.overrides = args.contents;
-    })
-    ipcRenderer.on('vendorCheckDiffs', (event, args) => {
+    });
+    ipcRenderer.on("vendorCheckDiffs", (event, args) => {
       this.vendorCheckDiffs = args.contents;
-    })
-    ipcRenderer.on('selectedMagento2ProjectDir', (event, args) => {
+    });
+    ipcRenderer.on("selectedMagento2ProjectDir", (event, args) => {
       this.selectedMagento2ProjectDir = args.dir;
-    })
+    });
   },
-  updated: function () {
-    if (this.type === 'Plugin' && this.methodName !== null) {
-      let highlightElement = [...document.querySelectorAll('span.function.token')].map(span => span.innerText === this.methodName ? span : false).filter(Boolean).shift();
+  updated: function() {
+    if (this.type === "Plugin" && this.methodName !== null) {
+      let highlightElement = [
+        ...document.querySelectorAll("span.function.token"),
+      ]
+        .map((span) => (span.innerText === this.methodName ? span : false))
+        .filter(Boolean)
+        .shift();
       if (highlightElement) {
-        highlightElement.classList.add('methodHighlight');
+        highlightElement.classList.add("methodHighlight");
       }
     }
   },
@@ -145,7 +181,7 @@ export default {
     customFileContentHighlighter(code) {
       return highlight(code, languages[this.customFileType]);
     },
-    loadOverride: function (event) {
+    loadOverride: function(event) {
       if (event.target.value in this.overrides) {
         // how to negate "in" operator??
       } else {
@@ -153,20 +189,24 @@ export default {
         return;
       }
       let methodName = null;
-      let [type, vendorFilePath, customFilePath] = this.overrides[event.target.value];
+      let [type, vendorFilePath, customFilePath] = this.overrides[
+        event.target.value
+      ];
       this.selectedOverride = vendorFilePath;
       this.type = type;
 
-      if (type === 'Preference' || type === 'Plugin') {
+      if (type === "Preference" || type === "Plugin") {
         // load authoritative classmap from Composer autoloader
         if (!this.classMap) {
-          this.classMap = JSON.parse(fs.readFileSync(this.selectedMagento2ProjectDir + '/classmap.json'));
+          this.classMap = JSON.parse(
+            fs.readFileSync(this.selectedMagento2ProjectDir + "/classmap.json")
+          );
         }
 
         // find class name
         let className = customFilePath;
-        if (type === 'Plugin') {
-          [className, methodName] = customFilePath.split('::');
+        if (type === "Plugin") {
+          [className, methodName] = customFilePath.split("::");
         }
 
         // find class
@@ -176,23 +216,24 @@ export default {
       }
 
       this.methodName = methodName;
-      this.copyPasteableFilePath = customFilePath.replace('data/', '');
+      this.copyPasteableFilePath = customFilePath.replace("data/", "");
       customFilePath = this.fixPath(customFilePath);
-      this.customFileType = customFilePath.split('.').pop();
-      if (this.customFileType === 'phtml') {
-        this.customFileType = 'php';
+      this.customFileType = customFilePath.split(".").pop();
+      if (this.customFileType === "phtml") {
+        this.customFileType = "php";
       }
       if (fs.existsSync(customFilePath)) {
         this.customFileContent = fs.readFileSync(customFilePath).toString();
       } else {
-        this.customFileContent = 'Could not find contents of ' + customFilePath;
+        this.customFileContent = "Could not find contents of " + customFilePath;
       }
 
-      this.vendorFileType = 'diff';
-      if (vendorFilePath in this.vendorCheckDiffs) {	
+      this.vendorFileType = "diff";
+      if (vendorFilePath in this.vendorCheckDiffs) {
         this.vendorFileContent = this.vendorCheckDiffs[vendorFilePath];
       } else {
-        this.vendorFileContent = 'Could not find diff info for ' + vendorFilePath;
+        this.vendorFileContent =
+          "Could not find diff info for " + vendorFilePath;
       }
 
       // Highlight the method name for plugins
@@ -202,11 +243,11 @@ export default {
       // }, 100);
     },
     fixPath(path) {
-	if (path.indexOf('/data/') > -1) {
-            path = path.split('magento2')[1];
-	}
-	return this.selectedMagento2ProjectDir + path;
-    }
-  }
-}
+      if (path.indexOf("/data/") > -1) {
+        path = path.split("magento2/")[1];
+      }
+      return this.selectedMagento2ProjectDir + "/" + path;
+    },
+  },
+};
 </script>
