7f490d9e18b009cef4df200ef0d9d10c170a2810
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2021-11-24 07:31:03 +0100

    Do not open devtools by default

--- src/background.js
+++ src/background.js
@@ -45,7 +45,7 @@ function createWindow() {
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools({mode:'bottom'})
+    //if (!process.env.IS_TEST) win.webContents.openDevTools({mode:'bottom'})
   } else {
     createProtocol('app')
     // Load the index.html when not in development
